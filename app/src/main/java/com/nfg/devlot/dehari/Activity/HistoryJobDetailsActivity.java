4eebbcb1e5035db3f66d77140db4b8b61b805493
shahzad30403 <shahzad30403@gmail.com>
2018-04-08 22:37:26 +0500

    jobs activities and job details activities are added

---
+++ app/src/main/java/com/nfg/devlot/dehari/Activity/HistoryJobDetailsActivity.java
@@ -0,0 +1,15 @@
+package com.nfg.devlot.dehari.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.nfg.devlot.dehari.R;
+
+public class HistoryJobDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history_job_details);
+    }
+}
